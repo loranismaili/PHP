95cfb7e2af3fd44925cc9447abf6e01f5fa78bce
loranismaili <loranismaili1@gmail.com>
2025-05-05 12:07:11 +0200

    ushtrim commit 5/5/2025

---
+++ ushtrim.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <style>
+    li{
+    color: blue;
+    list-style-type: none;
+    text-decoration: solid;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bold;
+}
+ </style>
+</head>
+<body>
+    <ul>
+        <li>apple</li>
+        <li>grape</li>
+        <li>pear</li>
+        <li>banana</li>
+    </ul>
+    <?php
+    echo "This are fruits"
+    ?>
+
+</body>
+</html>
