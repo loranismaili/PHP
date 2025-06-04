49983ab5377877c905ebe3f764ad4e2bd582a5cf
loranismaili <loranismaili1@gmail.com>
2025-06-04 12:11:48 +0200

    lesson 9

---
+++ lesson9/add.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once('config.php');
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO user (name, username, email) VALUES (:name, :username, :email)";
+    $sqlQuery = $conn->prepare($sql);
+
+    $sqlQuery->bindParam(':name', $name);
+    $sqlQuery->bindParam(':username', $username);
+    $sqlQuery->bindParam(':email', $email);
+
+    $sqlQuery->execute();
+
+    echo "Data saved successfully";
+}
+
+?>
